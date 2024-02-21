e52734a77df5b4bc4deea19c9368faa8d09ed1a3
Anissa Nishioka <ani.nishioka@gmail.com>
2024-02-21 15:57:42 -0800

    implement root routing

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 @app.get('/')
-def form() {
-    """Returns madlibs form."""
-}
+def form():
+    """Returns madlibs form with input forms for each prompt in story."""
+    return render_template("questions.html", prompts=silly_story.prompts)
