a3622fb6d38e9cbdcecd4aa71481109691c94d99
andres-ordonezs <andresmw.89@gmail.com>
2024-02-21 19:48:39 -0500

    second commit

--- app.py
+++ app.py
@@ -8,7 +8,19 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+
 @app.get('/')
 def form():
     """Returns madlibs form with input forms for each prompt in story."""
     return render_template("questions.html", prompts=silly_story.prompts)
+
+
+@app.get("/results")
+def return_result():
+    """Returns the final text with the user's inputs"""
+
+# TODO: pass request.args
+    text = silly_story.get_result_text(
+        {item: request.args[item] for item in silly_story.prompts})
+# TODO: name text as result
+    return render_template('results.html', result=text)
