4be48592c74c216c52064f837633e40784f2b86b
Anissa Nishioka <ani.nishioka@gmail.com>
2024-02-21 17:08:40 -0800

    add story_menu route

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
+
+STORIES = {1: silly_story, 2: excited_story}
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
-
 @app.get('/')
+def story_menu():
+    return render_template("story_menu.html", stories=STORIES)
+
+@app.get('/<template>')
 def form():
     """Returns madlibs form with input forms for each prompt in story."""
     return render_template("questions.html", prompts=silly_story.prompts)
@@ -19,8 +24,5 @@ def form():
 def return_result():
     """Returns the final text with the user's inputs"""
 
-# TODO: pass request.args
-    text = silly_story.get_result_text(
-        {item: request.args[item] for item in silly_story.prompts})
-# TODO: name text as result
-    return render_template('results.html', result=text)
+    result = silly_story.get_result_text(request.args)
+    return render_template('results.html', result=result)
